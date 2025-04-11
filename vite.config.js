ccad2f251958bc24b2aa517810426640eb988e76
Septiphore <jordanlebouef@yahoo.com>
2025-04-11 01:19:02 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.', // This tells Vite where to look
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html', // 👈 Force correct HTML entry
+    },
+  },
 })
