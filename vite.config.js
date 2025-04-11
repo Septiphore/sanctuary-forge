ddbf653ce2dc0ec3471148f49c76ffb66ff68373
Septiphore <jordanlebouef@yahoo.com>
2025-04-11 02:02:48 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // 🔥 Add this line
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: 'public/index.html',
-    },
   },
 });
